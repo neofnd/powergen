e3d63edc33973f06b20d6057987cad96f918aa04
lemoncloud <lemoncloud@gmail.com>
2023-08-24 15:48:47 +0900

    test

---
+++ power/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import styled from 'styled-components';
+import './App.css';
+
+const ButtonMedium = styled.button`
+  background-color: red;
+`;
+function App() {
+  return (
+
+    <ButtonMedium>Medium</ButtonMedium>
+  );
+}
+
+export default App;
