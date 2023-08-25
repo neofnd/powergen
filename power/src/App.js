9f05a63d5699507db0e2c61495356c1ade921120
myommyo <lemoncloud@gmail.com>
2023-08-25 19:46:09 +0900

    test

--- power/src/App.js
+++ power/src/App.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import './App.css';
 
 const ButtonMedium = styled.button`
-  background-color: red;
+  background-color: blue;
 `;
 function App() {
   return (
